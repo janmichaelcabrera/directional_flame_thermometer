df4e7d9eaa3dd8c90fc7ddddffa44a93e5d4a9e3
janmichaelcabrera <janmichael.cabrera@gmail.com>
2019-08-06 16:37:53 -0500

    repo initialized

---
+++ dft_models.py
@@ -0,0 +1,4 @@
+#!/packages/python/anaconda3/bin python
+
+from __future__ import division
+import numpy as np
---
+++ tests/test.py
@@ -0,0 +1,9 @@
+#!/packages/python/anaconda3/bin python
+
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+import os, sys, time
+sys.path.append('../../')
+import differential_flame_thermometer as dft
+
