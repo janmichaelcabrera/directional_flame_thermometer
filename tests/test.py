ae1f8af3e4b31fd6a02a235e04e9eea426e42da7
janmichaelcabrera <janmichael.cabrera@gmail.com>
2020-10-12 06:35:12 -0500

    test.py comments added

--- tests/test.py
+++ tests/test.py
@@ -1,27 +1,39 @@
-from __future__ import division
-import numpy as np
 import matplotlib.pyplot as plt
-import os, sys
+import sys
 sys.path.append('../../')
 import directional_flame_thermometer as dft
 import pandas as pd
 
+# Read in test data
 df = pd.read_csv('data/1905-01_10.csv')
+
+# Pass data to variables
 Tf = df.tc_1.values
 Tb = df.tc_2.values
 time = df.time.values
 
+# Instantiate heat transfer coefficient objects for front and back plates
+h_f = dft.natural_convection(Tf)
+h_b = dft.natural_convection(Tb)
+
+# Set heat transfer coefficient model variables
 C = 0.65
 n = 0.25
 
-h_f = dft.natural_convection(Tf)
+# Pass model variables to heat transfer coefficient objects
 h_f.custom(C, n)
-
-h_b = dft.natural_convection(Tb)
 h_b.custom(C, n)
 
+# Pass variables and run model
 sensor_1 = dft.one_dim_conduction(Tf, Tb, time, h_f.h, h_b.h, model='one_d_conduction')
 
+# Plot results
 plt.figure()
-plt.plot(time, sensor_1.q_inc)
+plt.plot(time, sensor_1.q_inc, label='$q_{inc}$')
+plt.plot(time, sensor_1.q_net, label='$q_{net}$')
+plt.plot(time, sensor_1.q_conv, label='$q_{conv}$')
+plt.plot(time, sensor_1.q_rad, label='$q_{rad}$')
+plt.xlabel('Time (s)', fontsize=14)
+plt.ylabel('Heat Flux (kW/m$^2$)', fontsize=14)
+plt.legend(loc=0)
 plt.show()
